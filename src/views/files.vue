b6ae73d056ee2171ae7383783168b7f6dc469d08
bei <1850055742@qq.com>
2021-11-23 10:01:29 +0800

    add directory icon

--- src/views/files.vue
+++ src/views/files.vue
@@ -1,10 +1,21 @@
 <template>
-  <div>
+  <div class="app-files">
     <el-button @click="handleSelectDir">选择目录</el-button>
     <el-table :data="tableData">
+      <el-table-column prop="isDirectory" label="" width="180">
+        <template #default="{ row }">
+          <folder-icon
+            v-if="row.isDirectory"
+            class="app-files__file-icon"
+          ></folder-icon>
+        </template>
+      </el-table-column>
       <el-table-column prop="name" label="文件名" width="180" />
-      <el-table-column prop="isDirectory" label="目录" width="180" />
-      <el-table-column prop="lastModify" label="上次修改时间" width="180" />
+      <el-table-column prop="lastModify" label="上次修改时间" width="180">
+        <template #default="{ row }">
+          {{ dateFormatter(row) }}
+        </template>
+      </el-table-column>
       <el-table-column prop="size" label="大小" width="180" />
     </el-table>
   </div>
@@ -12,11 +23,16 @@
 
 <script lang="ts">
 import { defineComponent, ref } from 'vue';
+import dayjs from 'dayjs';
 import { useFileSystem } from '../composable/fileSystem';
 import { AppFile } from '../types';
+import FolderIcon from '../assets/icons/default_folder.svg?component';
 
 export default defineComponent({
   name: 'Files',
+  components: {
+    FolderIcon,
+  },
   setup() {
     const { selectDirectory, getDirInfo } = useFileSystem();
     const tableData = ref<AppFile[]>([]);
@@ -31,14 +47,14 @@ export default defineComponent({
           const file = await handle.getFile();
           data.push({
             name: file.name,
-            isDirectory: true,
+            isDirectory: false,
             lastModify: file.lastModified,
             size: file.size,
           });
         } else {
           data.push({
             name: handle.name,
-            isDirectory: false,
+            isDirectory: true,
             lastModify: null,
             size: null,
           });
@@ -48,10 +64,29 @@ export default defineComponent({
       tableData.value = data;
     };
 
+    const dateFormatter = (row: AppFile): string => {
+      if (row.lastModify) {
+        return dayjs(row.lastModify).format('YYYY-MM-DD hh:mm:ss');
+      } else {
+        return '';
+      }
+    };
+
     return {
       tableData,
       handleSelectDir,
+      dateFormatter,
     };
   },
 });
 </script>
+
+<style lang="scss">
+.app-files {
+  &__file-icon {
+    $r: 30px;
+    width: $r;
+    height: $r;
+  }
+}
+</style>
--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import Components from 'unplugin-vue-components/vite';
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
+import svgLoader from 'vite-svg-loader';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,5 +11,6 @@ export default defineConfig({
     Components({
       resolvers: [ElementPlusResolver()],
     }),
+    svgLoader(),
   ],
 });
